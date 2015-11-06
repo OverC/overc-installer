87a3fa6efce9d91b3a7a60177316652c44944517
T.O. Radzy Radzykewycz <radzy@windriver.com>
2015-11-06 09:52:43 -0800

    Move mount point to temp dir
    Create a temporary directory to mount the filesystem on.  This is
    a necessary requirement for the installers to be reentrant.
    Some users may wish to run multiple installers simultaneously,
    for example to create image files for two different boards or to
    create an installer image file and a bootable image file, or to
    create bootable image files for different sized storage devices.
    
    Signed-off-by: T.O. Radzy Radzykewycz <radzy@windriver.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- installers/simple-install-md0.sh
+++ installers/simple-install-md0.sh
@@ -15,14 +15,18 @@ fdisk /dev/sdb < ./fdisk-b.txt
 mdadm --create /dev/md0 -n2 -l0 /dev/sda3 /dev/sdb2
 mkfs.ext4 -v -E stride=128 /dev/md0
 
-mkdir /z
-mount -v /dev/md0 /z
+if [ -z "${TMPMNT}" ]; then
+    TMPMNT=`mktemp -d /tmp/pulsarmountXXXXX`
+    export TMPMNT
+fi
+mkdir ${TMPMNT}
+mount -v /dev/md0 ${TMPMNT}
 
 mkfs.ext4 /dev/sda1
 mkswap /dev/sda2
 mkswap /dev/sdb1
 
 # Mount the boot partition:
-cd /z
+cd ${TMPMNT}
 mkdir boot
 mount -v /dev/sda1 boot
