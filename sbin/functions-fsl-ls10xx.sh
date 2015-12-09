d078723404001f0c3be5826eb85ca194e8b7db26
Guojian Zhou <guojian.zhou@windriver.com>
2015-12-09 17:30:49 +0800

    overc-installer: Embed the u-boot environment parameters into the image
    Automatically install the u-boot environment parameters into the image
    special sectors. It will make sure that u-boot could load the kernel and
    boot with the suitable command-line parameters by default.
    
    Signed-off-by: Guojian Zhou <guojian.zhou@windriver.com>
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- sbin/functions-fsl-ls10xx.sh
+++ sbin/functions-fsl-ls10xx.sh
@@ -5,6 +5,7 @@ install_bootloader()
 	local mountpoint="$2"
 	local bootloader="$3"
 	local boardname="$4"
+	local bootloader_env="$5"
 
 	if ! [ -e "$bootloader" ]; then
 		debugmsg ${DEBUG_CRIT} "INFO: didn't find bootloader file $bootloader"
@@ -15,4 +16,9 @@ install_bootloader()
 	BS=512
 	SEEK=8
 	dd if=$bootloader of=/dev/$device bs=$BS seek=$SEEK conv=notrunc oflag=sync
+	if [ -e "$bootloader_env" ]; then
+		#put the u-boot env into the location from 2048th section of boot device
+		SEEK=2048
+		dd if=$bootloader_env of=/dev/$device bs=$BS seek=$SEEK conv=notrunc oflag=sync
+	fi
 }
