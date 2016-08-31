b603188e21a4904253860e054323dd1ccf494001
Feng Mu <Feng.Mu@windriver.com>
2016-08-31 22:20:26 +0800

    installer: add interactive configuration process
    Currently user must define a configuration file to lead the
    installation process. In order to reduce the learning complexity of the
    configuration items, we introduce this interactive process to guide the
    user to genarate a configuration file and use that during the following
    installation process.
    
    This interactive process is triggered if no configuration file is
    provided through the commandline or if interactive parameter
    --interactive or -i is specifically provided through commandline.
    
    The genarated configuration file can be reused later by passing through
    the --config parameter.
    
    Signed-off-by: Feng Mu <Feng.Mu@windriver.com>

---
+++ lib/prompts/0004_user_config.sh
@@ -0,0 +1,49 @@
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 2 as
+#  published by the Free Software Foundation.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#  See the GNU General Public License for more details.
+
+# User related prompts
+
+user_config()
+{
+	local tmpfile=$(mktemp)
+	# Add new user
+	dialog --no-cancel --inputbox "Create new user(leave blank if not wanted):" 8 60 2>$tmpfile
+	username=$(cat $tmpfile)
+	if [ -n "$username" ]; then
+		passwd1="" passwd2="" passwdprompt="Please input password:"
+		while [ -z "$passwd1" ] || [ -z "$passwd2" ] || [ "$passwd1" != "$passwd2" ]; do
+			dialog --no-cancel --inputbox "$passwdprompt" 8 40 2>$tmpfile
+			passwd1=$(cat $tmpfile)
+			dialog --no-cancel --inputbox "Please confirm password:" 8 40 2>$tmpfile
+			passwd2=$(cat $tmpfile)
+			passwdprompt="Password mismatch! Please re-input password:"
+		done
+		cryptpasswd=$(python -c "import crypt; print crypt.crypt(\"$passwd1\", \"\$6\$\")")
+		cat <<EOF >> ${tmppuppet}
+group { "$username":
+	name => "$username",
+	ensure => present,
+}
+
+user { "$username":
+	ensure => present,
+	gid => "$username",
+	groups => ["users"],
+	membership => minimum,
+	shell => "/bin/bash",
+	require => [Group["$username"]],
+	password => '$cryptpasswd',
+}
+
+EOF
+	fi
+
+	rm $tmpfile
+}
+
